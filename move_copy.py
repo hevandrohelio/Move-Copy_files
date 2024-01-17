bcd2a615560c360aa385d118576f85f5a8f2624f
Hevandro Hélio <68600487+hevandrohelio@users.noreply.github.com>
2024-01-17 11:11:10 -0300

    Verifica se diretório de origem existe

--- move_copy.py
+++ move_copy.py
@@ -12,17 +12,28 @@ class Mc_file:
         self.all_ex_files_dir = []
         for file_ex in self.usefull_dir.glob('*'):
             self.all_ex_files_dir.append(file_ex)
-        self.extencions = {filepath.suffix[1:] for filepath in self.usefull_dir.glob('*')}
+        self.extensions = {filepath.suffix[1:] for filepath in self.usefull_dir.glob('*')}
 
     def copy(self):
-        for e in self.extencions:
-            os.mkdir(fr'{self.new_dir}\{e}_files')
-            for f in self.dir_files:
-                if e in f:
-                    shutil.copy(fr'{self.dir}\{f}', fr'{self.new_dir}\{e}_files')
+        if not os.path.isdir(self.new_dir):
+            os.mkdir(self.new_dir)
+        if os.path.isdir(self.dir):
+            for e in self.extensions:
+                os.mkdir(fr'{self.new_dir}\{e}_files')
+                for f in self.dir_files:
+                    if e in f:
+                        shutil.copy(fr'{self.dir}\{f}', fr'{self.new_dir}\{e}_files')
+        else:
+            return 'Origin Directory not found.'
+
     def move(self):
-        for e in self.extencions:
-            os.mkdir(fr'{self.new_dir}\{e}_files')
-            for f in self.dir_files:
-                if e in f:
-                    shutil.move(fr'{self.dir}\{f}', fr'{self.new_dir}\{e}_files')
+        if not os.path.isdir(self.new_dir):
+            os.mkdir(self.new_dir)
+        if os.path.isdir(self.dir):
+            for e in self.extensions:
+                os.mkdir(fr'{self.new_dir}\{e}_files')
+                for f in self.dir_files:
+                    if e in f:
+                        shutil.move(fr'{self.dir}\{f}', fr'{self.new_dir}\{e}_files')
+        else:
+            return 'Origin Directory not found.'
